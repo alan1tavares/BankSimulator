94c0fd7e42d03dd7f0a0c80804dde3a92a1e921d
Alan Tavares <alan1tavares@gmail.com>
2021-01-10 00:14:12 -0300

    :sparkles: Adicionado a classe de transaçao
    Criado os metodos da conta de depositar e sacar.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace BankSimulator
             var account = new BankAccount("Joao", 1000);
             Console.WriteLine($"Account {account.Number} was created" +
                 $" for {account.Owner} with {account.Balance} initial balance." );
+            
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -50);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caugh creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
